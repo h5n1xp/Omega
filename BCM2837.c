22b7c98c3176be0c6b16ebed093833db1bd98da8
h5n1xp <h5n1xp@gmail.com>
2019-02-20 08:06:09 +0000

    Add files via upload
    Experimental idea, add RaspberryPi chip emulation to the emulator so that I can write AmigaOS drivers for the RaspberryPi
     and test without needing to be running on a RaspberryPi...

---
+++ BCM2837.c
@@ -0,0 +1,15 @@
+//
+//  BCM2837.c
+//  The Omega Project
+//  https://github.com/h5n1xp/Omega
+//
+//  Created by Matt Parsons on 20/02/2019.
+//  Copyright © 2019 Matt Parsons. All rights reserved.
+//  <h5n1xp@gmail.com>
+//
+//
+//  This Source Code Form is subject to the terms of the
+//  Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+//  with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "BCM2837.h"
