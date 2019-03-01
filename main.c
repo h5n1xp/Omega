6ef14df127d9b6754145442903cceea9d41b8c74
h5n1xp <h5n1xp@gmail.com>
2019-03-01 07:58:43 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -90,7 +90,7 @@ int main(int argc, char * argv[]) {
     }
     
     close(fd);
-    
+    fd=0;
     
     //Floppy disk
     if(argc>2){
@@ -101,7 +101,7 @@ int main(int argc, char * argv[]) {
         fd = open("/Users/Shared/uae/WORKBENCH/WB-1.3.adf",O_RDONLY);
         //fd = open("/Users/Shared/uae/DosUae/DLXP4.ADF",O_RDONLY);
         //fd = open("/Users/Shared/uae/DosUae/DISK1.ADF",O_RDONLY);
-        //fd = open("/Users/Shared/uae/DosUae/Fright Night.adf",O_RDONLY);
+        fd = open("/Users/Shared/uae/DosUae/Fright Night.adf",O_RDONLY);
         //fd = open("/Users/Shared/uae/DosUae/JUGGLER.ADF",O_RDONLY);
         //fd = open("/Users/Shared/uae/DosUae/amiga_500a_Tutorial_Disk.ADF",O_RDONLY);
         //fd = open("/Users/Shared/uae/DosUae/CuAmiga15.adf",O_RDONLY);
@@ -114,8 +114,9 @@ int main(int argc, char * argv[]) {
     }else{
         ADF2MFM(fd, floppyInit(0));
         close(fd);
+
     }
-    
+    fd=0;
     
     //DF1
     //Floppy disk
@@ -130,13 +131,9 @@ int main(int argc, char * argv[]) {
         //fd = open("/Users/Shared/uae/DosUae/Fright Night.adf",O_RDONLY);
         //fd = open("/Users/Shared/uae/DosUae/JUGGLER.ADF",O_RDONLY);
         //fd = open("/Users/Shared/uae/DosUae/amiga_500a_Tutorial_Disk.ADF",O_RDONLY);
-        fd = open("/Users/Shared/uae/DosUae/af025_1991_08_d025.adf",O_RDONLY);
+        //fd = open("/Users/Shared/uae/DosUae/af025_1991_08_d025.adf",O_RDONLY);
         //fd = open("/Users/Shared/uae/DosUae/CuAmiga15.adf",O_RDONLY);
         //fd = open("/Users/Shared/uae/WORKBENCH/EX-1_3.adf",O_RDONLY);
-        
-        if(fd==0){
-            printf("no disk in DF1:\n");
-        }
         
     }
     
